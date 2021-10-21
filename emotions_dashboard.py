1d85d4c0b2d9381a85505196d8fe9b6fd59c64ca
pabloinsente <pcaceres@wisc.edu>
2021-10-21 17:20:09 -0500

    expand

--- emotions_dashboard.py
+++ emotions_dashboard.py
@@ -17,8 +17,6 @@ def load_data(path1, path2):
     
     return df, df_labels
 
-df, df_labels = load_data('clean_data/forced_choice_emotion_uw_students.csv', 'data/emotion_labels.csv')
-
 @st.cache(persist=True, allow_output_mutation=True)
 def count_freq_labels(df, X="all" ):
     if X == "all":
@@ -105,6 +103,8 @@ def simple_count_bar(
 st.write("""
 ## Demographics
 """)
+
+df, df_labels = load_data('clean_data/forced_choice_emotion_uw_students.csv', 'data/emotion_labels.csv')
 
 #####################
 ### - sex chart - ###
@@ -398,32 +398,3 @@ with col2:
 st.write("""
 ## Free-labeling categorization results
 """)
-
-df, df_label = load_data('/clean_data/free_choice_emotion_uw_students_overall.csv', '/data/emotion_labels_free_choice.csv')
-df_label['url'] = df_label['url'].astype(str)
-
-# replicate rows by 4 to match with free responses
-df_labels = pd.DataFrame(np.repeat(df_label.values, 4, axis=0))
-df_labels.columns = df_label.columns
-
-bars = alt.Chart(df, title=title).mark_bar().encode(
-    alt.X('percent:Q'),
-    y=alt.Y('emotion:N', sort='-x')
-    )
-
-text = bars.mark_text(
-align='left',
-baseline='middle',
-dx=3,  # Nudges text to right so it doesn't appear on top of the bar
-fontSize=12
-).encode(
-    alt.Text(X)
-)
-
-chart_free_overall= (bars + text).configure_axis(
-    labelFontSize=12,
-    titleFontSize=12).properties(
-        width=300, 
-        height=500)
-
-st.altair_chart(chart_free_overall, use_container_width=True)
